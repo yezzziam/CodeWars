923da1a874d34e0bc6ba4687d7ed68824d525f53
yezzziam <yezzziam@gmail.com>
2020-09-29 15:02:33 +0300

    First kata added.

---
+++ CodeWars.playground/Pages/RevRot.xcplaygroundpage/Contents.swift
@@ -0,0 +1,18 @@
+//: [Previous](@previous)
+
+import Foundation
+
+func revRot(_ s: String, _ c: Int) -> String {
+    
+    if c <= 0 || s.isEmpty || c > s.count {
+        return ""
+    }
+    
+    return "sup"
+}
+
+revRot("123", 4)
+//revrot("123456987654", 6) --> "234561876549"
+
+
+//: [Next](@next)
