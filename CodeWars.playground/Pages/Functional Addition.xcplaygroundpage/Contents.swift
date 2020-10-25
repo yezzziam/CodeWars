a6c8ae3cd30af58e848034795b9efb2c3372002e
yezzziam <yezzziam@gmail.com>
2020-10-25 17:15:56 +0200

    Functional Addition Kata Added

---
+++ CodeWars.playground/Pages/Functional Addition.xcplaygroundpage/Contents.swift
@@ -0,0 +1,13 @@
+//: [Previous](@previous)
+/* https://www.codewars.com/kata/538835ae443aae6e03000547/train/swift */
+
+import Foundation
+
+func add(_ n: Int) -> ((Int) -> Int) {
+    return { n + $0 }
+}
+
+add(0)(-15)
+
+
+//: [Next](@next)
