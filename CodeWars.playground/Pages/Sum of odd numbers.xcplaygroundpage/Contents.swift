faa039f9cc03de55ed376a7aefbd646063710f81
yezzziam <yezzziam@gmail.com>
2020-10-26 13:33:53 +0200

    Update to workspace

--- CodeWars.playground/Pages/Sum of odd numbers.xcplaygroundpage/Contents.swift
+++ CodeWars.playground/Pages/Sum of odd numbers.xcplaygroundpage/Contents.swift
@@ -4,16 +4,16 @@
 import Foundation
 
 func rowSumOddNumbers(_ row: Int) -> Int {
-//    var result = 1
-//    for i in 0..<row {
-//        result += (2 * i)
-//    }
-//    var total = result
-//    for _ in 0..<row {
-//        total += result
-//    }
-//    return total-1
-    row*row*row
+    var result = 1
+    for i in 0..<row {
+        result += (2 * i)
+    }
+    var total = result
+    for _ in 0..<row {
+        total += result
+    }
+    return total-1
+//    row*row*row
 }
 
 rowSumOddNumbers(101)
