f23ca5c1da659ae8e850196e749cb1b9fc781243
yezzziam <yezzziam@gmail.com>
2020-12-02 18:12:35 +0200

    Added Direction Reduction Kata

---
+++ CodeWars.playground/Pages/Numericals of a String.xcplaygroundpage/Contents.swift
@@ -0,0 +1,12 @@
+//: [Previous](@previous)
+
+/* https://www.codewars.com/kata/5b4070144d7d8bbfe7000001/train/swift */
+
+import Foundation
+
+func numericals(_ str: String) -> String {
+    return ""
+}
+
+
+//: [Next](@next)
