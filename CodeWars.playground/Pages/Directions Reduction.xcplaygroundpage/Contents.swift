d487bdd29cf9bfc769134e63104ee58e59f6a4fc
yezzziam <yezzziam@gmail.com>
2020-12-02 18:11:59 +0200

    Finished Direction Reduction Kata

--- CodeWars.playground/Pages/Directions Reduction.xcplaygroundpage/Contents.swift
+++ CodeWars.playground/Pages/Directions Reduction.xcplaygroundpage/Contents.swift
@@ -4,7 +4,32 @@
 import Foundation
 
 func dirReduc(_ arr: [String]) -> [String] {
-    return [""]
+    var arr = arr
+    let indexToDelete = { (arr: [String]) -> Int? in
+        for index in 0..<arr.count - 1 {
+            if arr[index] == "NORTH" && arr[index+1] == "SOUTH" {
+                return index
+            }
+            
+            if arr[index] == "SOUTH" && arr[index+1] == "NORTH" {
+                return index
+            }
+            
+            if arr[index] == "EAST" && arr[index+1] == "WEST" {
+                return index
+            }
+            
+            if arr[index] == "WEST" && arr[index+1] == "EAST" {
+                return index
+            }
+        }
+        return nil
+    }
+    
+    while let index = indexToDelete(arr) {
+        arr.removeSubrange(index...index+1)
+    }
+    return arr
 }
 
 print(dirReduc(["NORTH", "SOUTH", "SOUTH", "EAST", "WEST", "NORTH", "WEST"]))
